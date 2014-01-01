6490acbee32c7178ab8ef377526a24ae1d7ae35c
shaigiv <shai.givati@gmail.com>
2014-01-01 15:58:03 +0200

    קבוצות

--- the-site/Json_Manager.js
+++ the-site/Json_Manager.js
@@ -1,4 +1,4 @@
-﻿var missions = {};
+﻿
 
 function JsonMan() {
 
@@ -36,7 +36,7 @@ function JsonMan() {
         this.sendAjax(to_url, func);
     }
 
-     this.get_question = function (id, func) {
+    this.get_question = function (id, func) {
         var to_url = this.domain + "get_post&post_id=" + id + "&post_type=question";
 
         this.sendAjax(to_url, func);
@@ -50,22 +50,21 @@ function JsonHandler() {
     var self = this;
     self.saver;
     self.missionCounter = 0;
+    self.questionCounter = 0;
     self.question;
 
-    this.get_activity_day_handler = function (json) {
-        var b = [];
-        var saver = json;
-        a = saver.post.custom_fields.missions[0].split('"')
-        for (i = 1; i < a.length; i += 2) {
-            b.push(a[i]);
-            jsonManager.get_mission(a[i], jsonHandler.get_mission_handler);
+    this.get_activity_day_handler = function (missionsStr) {
+        var missionsPostId = [];
+        var tempArr = missionsStr.post.custom_fields.missions[0].split('"')
+        for (i = 1; i < tempArr.length; i += 2) {
+            missionsPostId.push(tempArr[i]);
+            jsonManager.get_mission(tempArr[i], jsonHandler.get_mission_handler);
         }
-        console.log(b);
+        console.log(missionsPostId);
 
     }
 
     this.get_mission_handler = function (mission) {
-        console.log(mission);
 
         self.missionCounter++;
         var missionId = 'mission' + self.missionCounter;
@@ -81,7 +80,7 @@ function JsonHandler() {
 
         switch (mission.post.type) {
             case "take-photo-mission":
-                missions[missionId].numOfPhotosRequired = 5;//change+++++++++++++++++++++++++++++++++++
+                missions[missionId].numOfPhotosRequired = 5; //change+++++++++++++++++++++++++++++++++++
                 break;
 
             case "navigation-mission":
@@ -93,28 +92,31 @@ function JsonHandler() {
                 missions[missionId].medicine[0] = {};
                 missions[missionId].medicine[1] = {};
                 missions[missionId].medicine[2] = {};
-                
+
                 missions[missionId].medicine[0].name = mission.post.custom_fields["wpcf-better-medicine-name"][0];
                 missions[missionId].medicine[0].description = mission.post.custom_fields["wpcf-better-medicine-description"][0];
                 missions[missionId].medicine[0].audience = mission.post.custom_fields["wpcf-better-medicine-audience"][0];
 
-                missions[missionId].medicine[1].name = mission.post.custom_fields["wpcf-prevent-medicine-name"][0];;
+                missions[missionId].medicine[1].name = mission.post.custom_fields["wpcf-prevent-medicine-name"][0]; ;
                 missions[missionId].medicine[1].description = mission.post.custom_fields["wpcf-prevent-medicine-description"][0];
                 missions[missionId].medicine[1].audience = mission.post.custom_fields["wpcf-prevent-medicine-audience"][0];
 
-                missions[missionId].medicine[2].name = mission.post.custom_fields["wpcf-saver-medicine-name"][0];;
+                missions[missionId].medicine[2].name = mission.post.custom_fields["wpcf-saver-medicine-name"][0]; ;
                 missions[missionId].medicine[2].description = mission.post.custom_fields["wpcf-saver-medicine-description"][0];
                 missions[missionId].medicine[2].audience = mission.post.custom_fields["wpcf-saver-medicine-audience"][0];
                 break;
 
             case "quiz-mission":
+                questions = [];
+                self.questionCounter = 0;
                 var b = [];
                 a = mission.post.custom_fields.questions[0].split('"')
                 for (i = 1; i < a.length; i += 2) {
                     b.push(a[i]);
                     jsonManager.get_question(a[i], self.get_question_handler); //jsonHandler.get_question
                 }
-                console.log(b);
+
+                missions[missionId].quiz = questions;
 
                 break;
 
@@ -135,12 +137,18 @@ function JsonHandler() {
                 break;
         }
 
+        console.log(missions);
     }
 
     this.get_question_handler = function (question) {
-        console.log("GOOD QUESTIOn");
-        console.log(question);
-
+        questions[self.questionCounter] = [];
+        questions[self.questionCounter][0] = question.post.custom_fields["wpcf-question"][0];
+        questions[self.questionCounter][1] = question.post.custom_fields["wpcf-answer-1"][0];
+        questions[self.questionCounter][2] = question.post.custom_fields["wpcf-answer-2"][0];
+        questions[self.questionCounter][3] = question.post.custom_fields["wpcf-answer-3"][0];
+        questions[self.questionCounter][4] = question.post.custom_fields["wpcf-answer-4"][0];
+        questions[self.questionCounter][5] = parseInt(question.post.custom_fields["wpcf-right-answer"][0]);
+        self.questionCounter++;
     }
 } //JsonHandler()
 
@@ -148,6 +156,8 @@ var jsonManager = new JsonMan();
 
 var jsonHandler = new JsonHandler();
 
+var missions = {};
+var questions = [];
 jsonManager.get_activity_day(18, jsonHandler.get_activity_day_handler);
 
 //איך לדאוג שיעבור לפי הסדר?
--- wp-config.php
+++ wp-config.php
@@ -19,13 +19,13 @@
 define('DB_NAME', 'rot_cube');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'admin');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'cambium#1');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'www.rothschild-kube.com.tigris.nethost.co.il');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
